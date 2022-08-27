9ac1dfabfc83a89fe7a7315d8312019532edc7d4
NforLev <105176489+NforLev@users.noreply.github.com>
2022-08-27 13:47:14 -0300

    Files uploaded

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -11,26 +11,22 @@ let clear = document.getElementById("clearAll")
 let back = document.getElementById("back")
 let resultDisplay = document.getElementById("result")
 let memoryDisplay = document.getElementById("memoryDisplay")
-
-//operands
+    //operands
 let multiply = document.getElementById("multi")
 let divide = document.getElementById("divide")
 let sum = document.getElementById("sum")
 let minus = document.getElementById("minus")
-//numbers
-let btn1 = document.getElementById("1")
-let btn2 = document.getElementById("2")
-let btn3 = document.getElementById("3")
-let btn4 = document.getElementById("4")
-let btn5 = document.getElementById("5")
-let btn6 = document.getElementById("6")
-let btn7 = document.getElementById("7")
-let btn8 = document.getElementById("8")
-let btn9 = document.getElementById("9")
-let btn0 = document.getElementById("0")
-let btnComa = document.getElementById(".")
+    //numbers
+let numBtns = document.querySelectorAll(".numbers")
+numBtns.forEach(numBtns=> numBtns.addEventListener("click", input))
 
-//onClick
+//listeners
+clear.addEventListener("click", clearAll)
+equalbtn.addEventListener("click", equal)
+back.addEventListener("click", erase)
+
+
+//onClick events
 multiply.onclick = () => {
     equal()
     currentMode = "multiply"
@@ -54,21 +50,6 @@ minus.onclick = () => {
     memoryDisplay.textContent="Minus"
 }
 
-//listeners
-btn1.addEventListener("click", input)
-btn2.addEventListener("click", input)
-btn3.addEventListener("click", input)
-btn4.addEventListener("click", input)
-btn5.addEventListener("click", input)
-btn6.addEventListener("click", input)
-btn7.addEventListener("click", input)
-btn8.addEventListener("click", input)
-btn9.addEventListener("click", input)
-btn0.addEventListener("click", input)
-clear.addEventListener("click", clearAll)
-btnComa.addEventListener("click", input)
-equalbtn.addEventListener("click", equal)
-back.addEventListener("click", erase)
 
 //functions 
     //get first and second pair of numbers
@@ -114,7 +95,7 @@ function newNums(){
     memoryDisplay.textContent=""
 }
 
-//function to clear all
+    //function to clear all
 function clearAll(){
     firstInput=""
     secondInput=""
@@ -124,9 +105,7 @@ function clearAll(){
     memoryDisplay.textContent=""
 }
 
-memoryDisplay.innerText=""
-
-//function to undo last input number
+    //function to delete last input number
 function erase(){
     if(firstInput!=""&&secondInput==""){
         let num = firstInput.toString()
